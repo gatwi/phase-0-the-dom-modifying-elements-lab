2795624a124fc9e044e1286f1eafafcdbff9986f
gatwi <faith.murithi@student.moringaschool.com>
2022-06-04 21:22:47 +0300

    commit to master

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement("h1")
+document.body.append(newHeader)
+newHeader.innerHTML = 'Gatwiri is the champion'
+newHeader.id = "#victory"
