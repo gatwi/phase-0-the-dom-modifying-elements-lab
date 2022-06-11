fe325a9b80b0afd2ac93bddae32efda6a0a2a61e
gatwi <faith.murithi@student.moringaschool.com>
2022-06-11 22:07:48 +0300

    commit to master

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 const main = document.getElementById('main')
 main.remove()
-
+main.innerHTML = "<h1>victory</h1><h3>Gatwiri is the champion</h3>"
 let newHeader = document.createElement("h1")
 document.body.append(newHeader)
 newHeader.innerHTML = 'Gatwiri is the champion'
 newHeader.id = "#victory"
+newHeader.className = "victory";
